62e941b3cd7e46e12172773402d7a307e5271d64
Ivan Nunes <ivangabriel633@gmail.com>
2022-02-02 23:57:11 -0300

    update calculadora

---
+++ projeto_calculadora/README.md
@@ -0,0 +1,26 @@
+<h1 align="center">Calculadora</h1>
+
+<p align="center">Calculadora funcional</p>
+<br>
+
+<div align="center"> <b>MODELO SEGUIDO</b>
+    <img src="..\projeto_calculadora\assets\images\calculadora-modelo.jpg" alt="modelo-seguido">
+</div>
+
+<hr>
+
+## 🚀 Tecnologias
+
+Este projeto foi desenvolvido utilizando as seguintes tecnologias:
+
+- ✔️ CSS
+
+- ✔️ HTML
+
+- ✔️ Javascript
+
+<br>
+
+<ul> Pages:
+<li><a href="https://ivanunes.github.io/estudos/projeto_calculadora/">Jogo da memória </a></li>
+</ul>
--- projeto_calculadora/scripts/calc.js
+++ projeto_calculadora/scripts/calc.js
@@ -1,3 +1,37 @@
 let hud = document.getElementById("tela");
+let resultado = [];
+let vez = 0;
+let operadores = ["+", "-"]
+let ligado = false;
 
-hud.innerHTML = "oi";
+function desligar(){
+    ligado = false;
+    resultado;
+}
+
+function ligar(){
+    ligado = true;
+}
+
+function mostrar(num) {
+    if (resultado.length != 7 && ligado != false) {
+        hud.innerHTML += num.value;
+        memoria(num.value);
+    }
+}
+
+function memoria(otNum) {
+    resultado[vez] = otNum;
+
+    console.log(resultado);
+
+    solucao();
+    vez++;
+}
+
+function solucao() {
+    for (rest of resultado){
+        console.log(rest);
+    }
+}
+
