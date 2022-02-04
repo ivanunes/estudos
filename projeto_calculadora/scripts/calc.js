d5fc48e76a786756b09b0154d4b96bd7b6f140ef
Ivan Nunes <ivangabriel633@gmail.com>
2022-02-03 22:05:24 -0300

    Semi finalização calculadora

--- projeto_calculadora/scripts/calc.js
+++ projeto_calculadora/scripts/calc.js
@@ -1,22 +1,34 @@
 let hud = document.getElementById("tela");
-let resultado = [];
+let resultado = ['', '', '', '', '', '', '', '', '', ''];
 let vez = 0;
 let operadores = ["+", "-"]
 let ligado = false;
 
+hud.innerHTML = "88888888";
+
 function desligar() {
     ligado = false;
-    resultado = [];
+    resultado = ['', '', '', '', '', '', '', '', '', ''];
     vez = 0;
-    hud.innerHTML = resultado;
+    hud.innerHTML = "88888888";
 }
 
 function ligar() {
-    ligado = true;
+    if (ligado == false && vez == 0) {
+        ligado = true;
+        hud.innerHTML = [];
+    }
+}
+
+function limparTudo() {
+    resultado = ['', '', '', '', '', '', '', '', '', ''];
+    vez = 0;
+    console.log(resultado);
+    hud.innerHTML = [];
 }
 
 function mostrar(num) {
-    if (resultado.length != 7 && ligado != false) {
+    if (resultado.length != 10 || ligado != false) {
         hud.innerHTML += num.value;
         memoria(num.value);
     }
@@ -30,21 +42,53 @@ function memoria(otNum) {
     vez++;
 }
 
+podeSeguir = false;
+
 function solucao() {
-    let vez1 = 0;
+    let num1 = resultado[0];
+    let num2 = resultado[1];
+    let num3 = resultado[2];
+    let num4 = resultado[3];
+    let num5 = resultado[4];
+    let num6 = resultado[5];
+    let num7 = resultado[6];
+    let num8 = resultado[7];
+    let num9 = resultado[8];
+    let num10 = resultado[9];
 
-    resultado.forEach(element => {
-        let h = eval(resultado[vez1].toString);
+    num1 == "%" ? num1 = "0" : resultado[0];
+    num2 == "%" ? num2 = "/ 100 *" : resultado[1], podeSeguir = true;;
+    num3 == "%" ? num3 = "/ 100 *" : resultado[2], podeSeguir = true;;
+    num4 == "%" ? num4 = "/ 100 *" : resultado[3], podeSeguir = true;;
+    num5 == "%" ? num5 = "/ 100 *" : resultado[4], podeSeguir = true;;
+    num6 == "%" ? num6 = "/ 100 *" : resultado[5], podeSeguir = true;;
+    num7 == "%" ? num7 = "/ 100 *" : resultado[6], podeSeguir = true;;
+    num8 == "%" ? num8 = "/ 100 *" : resultado[7], podeSeguir = true;;
+    num9 == "%" ? num9 = "/ 100 *" : resultado[8], podeSeguir = true;;
+    num10 =="%" ? num10 = "/ 100 *" : resultado[9], podeSeguir = true;;
 
-        console.log(resultado);
+    if (podeSeguir == true){
+        hud.innerHTML = parseFloat(eval(num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10));
+    }
+    
 
-        vez++;
-    });
+    ligado = false;
+}
 
+raiz(50);
+raiz(3);
 
+function raiz(number){
+    let contador = number;
+    let subt = 1;
+    let raizQ = 0;
 
+    while(!contador == 00){
+        contador -= subt;
+        subt += 2;
+        raizQ++;
+    }
 
-
-
+    console.log(raizQ);
 }
 
